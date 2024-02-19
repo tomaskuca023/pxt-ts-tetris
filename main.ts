c656ae4a7b44cfc749ae4245e99f6ea99f11a57b
tomaskuca023 <146720075+tomaskuca023@users.noreply.github.com>
2024-02-19 13:02:42 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+type Piece = {
+    x: number,
+    y: number
+}
+
+type Row = Array<boolean>
+type Display = Array<Row>
+
+const dData: Display = [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+]
+
+const dot: Piece = {
+    x: 2, y: 0
+}
+
+function refresh(display: Display):void {
+    for(let y: number = 0; y < 5; y++) {
+        for (let x: number = 0; x < 5; x++) {
+            if (display[y][x]) {
+                led.plot(x,y)
+            }
+            else {
+                led.unplot(x,y)
+            }
+        }
+    }
+}
+
+refresh(dData)
