083e8d82b1358e06f476cf58f756664c3826f768
tomaskuca023 <146720075+tomaskuca023@users.noreply.github.com>
2024-03-04 21:30:08 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+
+
 type Piece = {
     x: number,
     y: number
@@ -31,6 +33,48 @@ function refresh(display: Display):void {
     }
 }
 
-refresh(dData)
+basic.forever(function () {
+    refresh(dData)
+    console.log(dData[0])
+
+})
 
 
+
+let blockFalling: boolean = false
+let height: number = 5
+
+
+function block(display: Display):void {
+    blockFalling = true
+    let x: number = 2
+    for (let y: number = 0; y < height; y++) {
+ 
+        if (input.buttonIsPressed(Button.A)) {
+            x -=1
+            display [y-1][x+1] = false
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            x +=1
+            display [y-1][x-1] = false
+        }
+        display[y][x] = true
+        if (y >= 1) {
+            display [y-1][x] = false
+        }
+        basic.pause(1000)
+    }
+    blockFalling = false
+}
+
+
+
+basic.forever(function() {
+    
+if (!blockFalling) {
+    block(dData)
+}
+
+
+
+})
