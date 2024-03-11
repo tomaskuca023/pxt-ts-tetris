5e1e6231a3df12e998e40cbc1bf20b9dc6e860a0
tomaskuca023 <146720075+tomaskuca023@users.noreply.github.com>
2024-03-11 13:24:47 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,67 +1,121 @@
-
-
-type Piece = {
-    x: number,
-    y: number
-}
-
+type Piece = { x: number, y: number }
 type Row = Array<boolean>
 type Display = Array<Row>
-
+music.setVolume(100)
 const dData: Display = [
     [false, false, false, false, false],
     [false, false, false, false, false],
     [false, false, false, false, false],
     [false, false, false, false, false],
-    [false, false, false, false, false]
+    [false, false, false, false, false],
 ]
-const dot: Piece = {
-    x: 2, y: 0
-}
 
-function refresh(display: Display):void {
-    for(let y: number = 0; y < 5; y++) {
-        for (let x: number = 0; x < 5; x++) {
-            if (display[y][x]) {
-                led.plot(x,y)
+const dot: Piece = { x: 2, y: 0 }
+let speed: number = 500;
+
+function refresh(display: Display, dot: Piece): void {
+    for (let y = 0; y < 5; y += 1)
+        for (let x = 0; x < 5; x += 1)
+            if (display[y][x]) led.plot(x, y)
+            else led.unplot(x, y);
+    if (!gameOver) {
+        led.plot(dot.x, dot.y)     
+    }
+}
+let filled: number = 0
+let score: number = 0
+let level: number = 0
+let gameOver: boolean = false
+let scoreMenu: boolean = false
+basic.forever(function () {
+    if (!gameOver) {
+        refresh(dData, dot)
+        if (dot.y <= 3 && dData[dot.y + 1][dot.x]) {
+            dData[dot.y][dot.x] = true
+            dot.y = -1
+            dot.x = 2
+        }
+        if (dot.y == 4) {
+            dData[dot.y][dot.x] = true
+            dot.y = -1
+            dot.x = 2
+        }
+        dot.y = (dot.y + 1) % 5
+        if (dData[4][0] && dData[4][1] && dData[4][2] && dData[4][3] && dData[4][4]) {
+            for (let i: number = 0; i < 5; i++) {
+                dData[4][i] = false
+                dData[4][i] = dData[3][i]
+                dData[3][i] = dData[2][i]
+                dData[2][i] = dData[1][i]
+                dData[1][i] = dData[0][i]
+                dData[0][i] = false
             }
-            else {
-                led.unplot(x,y)
+            score += 1
+        }
+        for (let i: number = 0; i < dData.length; i++) {
+            if (dData[0][i]) {
+            if (!gameOver) {
+                basic.showLeds(`
+                . # . # .
+                . # . # .
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
             }
+            
+            gameOver = true}
+        
+        }
+        level = score
+        if (level > 19) level = 19
+        console.log(score)
+        console.log(speed-(level*25))
+        console.log(scoreMenu)
+        basic.pause(speed - (level * 25))
+    }
+    if (gameOver) {
+        if (scoreMenu) {
+            basic.showNumber(score)
+        }
+        else refresh(dData, dot)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (!gameOver) {
+        if (dot.x > 0) {
+            dot.x -= 1
+            refresh(dData, dot)
+        }
+        if (dData[dot.y][dot.x]) {
+            dot.x += 1
         }
     }
-}
-
-let pieceExists: boolean = false
-
-function pieceMovement(display: Display, piece: Piece):void {
-    pieceExists = true
-    if (input.buttonIsPressed(Button.A)) {
-        display[piece.y][piece.x] = false
-        piece.x -= 1
-        display[piece.y][piece.x] = true
-        basic.pause(100)
+    else {
+        if (scoreMenu) scoreMenu = false
+        else scoreMenu = true
     }
-    if (input.buttonIsPressed(Button.B)) {
-        display[piece.y][piece.x] = false
-        piece.x += 1
-        display[piece.y][piece.x] = true
-        basic.pause(100)
+})
+input.onButtonPressed(Button.B, function () {
+    if (!gameOver) {
+        if (dot.x < 4) {
+            dot.x += 1
+            refresh(dData, dot)
+        }
+        if (dData[dot.y][dot.x]) {
+            dot.x -= 1
+        }
     }
-    for (let i: number = 0; i < 5; i++) {
-        display[piece.y][piece.x] = false
-        piece.y += 1
-        display[piece.y][piece.x] = true
-        basic.pause(1000)
+    else {
+        filled = 0
+        score = 0
+        level = 0
+        dot.x = 2
+        dot.y = 0
+        gameOver = false
+        scoreMenu = false
+        for (let y = 0; y < 5; y += 1)
+            for (let x = 0; x < 5; x += 1)
+                dData[y][x] = false
     }
-    pieceExists = false
-}
-
-
-basic.forever(function () {
-    refresh(dData)
-    if (!pieceExists){
-        pieceMovement(dData, dot)
-    }
-
 })
